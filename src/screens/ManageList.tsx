81975048ab2bac3289155e5353db719869ae4815
syash2015 <syedasadhasan@gmail.com>
2024-12-10 10:52:51 +0530

    add dash

--- src/screens/ManageList.tsx
+++ src/screens/ManageList.tsx
@@ -170,7 +170,10 @@ const ManageList: React.FC = () => {
       {module.module_name === "string" ? "No Name Provided" : module.module_name}
     </DataTable.Cell>
     {/* <DataTable.Cell>{module.module_level || "N/A"}</DataTable.Cell> */}
-    <DataTable.Cell>{module.parent_module || "N/A"}</DataTable.Cell>
+    <DataTable.Cell>
+  {module.parent_module === "root module" ? "-------" : module.parent_module || "N/A"}
+    </DataTable.Cell>
+
     <DataTable.Cell>{module.url || "N/A"}</DataTable.Cell>
     <DataTable.Cell>
       {module.is_active ? "Active" : "Inactive"}
